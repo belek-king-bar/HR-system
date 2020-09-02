d2b2dd73a004ab7d1d88042e0da7062cd9437a3b
belek <belektolubaev@gmail.com>
2020-09-02 20:22:43 +0600

    Исправлены некоторые ошибки

--- hr_system/webapp/urls.py
+++ hr_system/webapp/urls.py
@@ -8,5 +8,5 @@ app_name = 'webapp'
 urlpatterns = [
     path('employees', EmployeeListView.as_view(), name='employee_list'),
     path('employee_data_create', EmployeeDataCreateView.as_view(), name='employee_data_create'),
-    path('wages', WagesView.as_view(), name='wages'),
+    path('wages', WagesView.as_view(), name='wage_list'),
 ]
